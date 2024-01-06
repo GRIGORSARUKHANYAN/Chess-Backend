eb10e4762cbbb535d0719cd8e8cf4cae51bfe386
khe4oyan <khe4oyan@gmail.com>
2024-01-07 03:23:54 +0400

    .

--- src/classes/ChessBoard.js
+++ src/classes/ChessBoard.js
@@ -2,20 +2,137 @@ const { chessBoardCreate } = require("./boardCreate");
 
 class ChessBoard {
   #board = chessBoardCreate();
-  #enPassant = {
-    black: { vertically: null, horizontally: null },
-    white: { vertically: null, horizontally: null },
-  };
-  #kingsPossition = {
-    black: { vertically: 0, horizontally: 4, check: false },
-    white: { vertically: 7, horizontally: 4, check: false },
-  };
-  #globalColor = 'white';
+  #checkWhite = false;
+  #checkBlack = false;
+  #checkMate = null; // white || black || null
 
-  constructor() {}
+  #whoseMove = 'white';
+
+  // constructor() {}
 
   get board() { return this.#board; }
+  get whoseMove() { return this.#whoseMove; }
 
+  // moveFigure(positionFrom, positionTo) {}
+
+  whereCanMove(vertically, horizontally) {
+    let result = [];
+
+    // check figure type
+    switch (this.#board[vertically][horizontally].pieces) {
+      case 'pawn': { result = this.#pawnMoves(vertically, horizontally); break; }
+      case 'rook': { result = this.#rookMoves(vertically, horizontally); break; }
+      case 'knight': { result = this.#knightMoves(vertically, horizontally); break; }
+      case 'bishop': { result = this.#bishopMoves(vertically, horizontally); break; }
+      case 'queen': { result = this.#queenMoves(vertically, horizontally); break; }
+      case 'king': { result = this.#kingMoves(vertically, horizontally); break; }
+      default: {
+        console.error('wrong figure');
+      }
+    }
+
+    return result;
+  }
+
+  #isValidSquarePosition(vertically, horizontally) {
+    return (vertically > -1 && vertically < 8 && horizontally > -1 && horizontally < 8) ? true : false;
+  }
+
+  #isEnemy(vertically, horizontally) {
+    if (this.#board[vertically][horizontally].color === null) {
+      return false;
+    }
+    if (this.#board[vertically][horizontally].color !== this.#whoseMove) {
+      return true;
+    }
+  }
+
+  #isFreeSquare(vertically, horizontally) {
+    if (this.#board[vertically][horizontally].pieces === null) {
+      return true;
+    }
+  }
+
+  #rookMoves(vertically, horizontally) {
+    const result = [];
+
+    for (let i = 1; i < 8; ++i) {
+      if (this.#isValidSquarePosition(vertically + i, horizontally)) {
+        if (this.#isEnemy(vertically + i, horizontally)) {
+          result.push({ vertically: vertically + i, horizontally });
+          break;
+        }
+        if (this.#isFreeSquare(vertically + i, horizontally)) {
+          result.push({ vertically: vertically + i, horizontally });
+        } else {
+          break;
+        }
+      } else {
+        break;
+      }
+    }
+
+    for (let i = 1; i < 8; ++i) {
+      if (this.#isValidSquarePosition(vertically - i, horizontally)) {
+        if (this.#isEnemy(vertically - i, horizontally)) {
+          result.push({ vertically: vertically - i, horizontally });
+          break;
+        }
+        if (this.#isFreeSquare(vertically - i, horizontally)) {
+          result.push({ vertically: vertically - i, horizontally });
+        } else {
+          break;
+        }
+      } else {
+        break;
+      }
+    }
+
+    for (let i = 1; i < 8; ++i) {
+      if (this.#isValidSquarePosition(vertically, horizontally + i)) {
+        if (this.#isEnemy(vertically, horizontally + i)) {
+          result.push({ vertically, horizontally: horizontally + i });
+          break;
+        }
+        if (this.#isFreeSquare(vertically, horizontally + i)) {
+          result.push({ vertically, horizontally: horizontally + i });
+        } else {
+          break;
+        }
+      } else {
+        break;
+      }
+    }
+
+    for (let i = 1; i < 8; ++i) {
+      if (this.#isValidSquarePosition(vertically, horizontally - i)) {
+        if (this.#isEnemy(vertically, horizontally - i)) {
+          result.push({ vertically, horizontally: horizontally - i });
+          break;
+        }
+        if (this.#isFreeSquare(vertically, horizontally - i)) {
+          result.push({ vertically, horizontally: horizontally - i });
+        } else {
+          break;
+        }
+      } else {
+        break;
+      }
+    }
+
+    return result;
+  }
+  #bishopMoves(vertically, horizontally) {
+    const result = [];
+
+    return result;
+  }
+  #knightMoves(vertically, horizontally) { }
+  #queenMoves(vertically, horizontally) {
+    return [...this.#bishopMoves(vertically, horizontally), ...this.#rookMoves(vertically, horizontally)];
+  }
+  #kingMoves(vertically, horizontally) { }
+  #pawnMoves(vertically, horizontally) { }
 };
 
 module.exports = { ChessBoard };
--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const { ChessBoard } = require("./classes/ChessBoard");
 
 app.use(cors());
 
-const board = new ChessBoard();
+const chessBoard = new ChessBoard();
 
 const server = http.createServer(app);
 const io = new Server(server, {
@@ -24,23 +24,45 @@ io.on("connection", (socket) => {
   allPlayers.push(socket.id);
 
   socket.on("start", _ => {
-    for (let i = 0; i < allPlayers.length; i++) {
-      socket.to(allPlayers[i]).emit("receive_step", board.board);
+    for (let i = 0; i < allPlayers.length; ++i) {
+      socket.to(allPlayers[i]).emit("receive_step", chessBoard.board);
     }
     if (allPlayers.length) {
-      socket.emit("receive_step", board.board);
+      socket.emit("receive_step", chessBoard.board);
     }
   });
 
-  socket.on("step", (data) => {});
-  socket.on("allow", (data) => {});
+  socket.on("step", (data) => {
+    const {from, to} = data;
+
+    // check figure type
+    // check can move
+    // if not, emit(false)
+    // else send board where moved
+
+    socket.emit("receive_step", false);
+  });
+
+  socket.on("showMoves", (figureFrom) => {
+    const {vertically, horizontally} = figureFrom;
+    let recieve_result = false;
+    
+    const board = chessBoard.board;
+    const whoseMove = chessBoard.whoseMove;
+
+    if (board[vertically][horizontally].color === whoseMove) {
+      recieve_result = chessBoard.whereCanMove(vertically, horizontally);
+    }
+
+    socket.emit("receive_showMoves", recieve_result);
+  });
 
   socket.on("disconnect", () => {
-    // do any task when player disconected
+    // do any task when player disconnected
   });
 });
 
 server.listen(3001, () => {
-  console.clear();
+  console.log('\n\n\n==================\n\n\n');
   console.log("SERVER IS RUNNING");
 });
